8e5e76f79b3343146aa085ca6775aaf10a11e009
villageworks <villageworks@gmail.com>
2024-08-22 08:43:40 +0900

    ver.0.00.40: schema.sqlおよびdata.sqlの追加

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS contact;
+DROP TABLE IF EXISTS company;
+DROP TABLE IF EXISTS user_auth;
+
+/**********************************/
+/* テーブル名: 取引先企業 */
+/**********************************/
+CREATE TABLE company(
+		id       SERIAL,
+		name     VARCHAR(255) NOT NULL,
+		industry TEXT,
+		rank     INTEGER,
+		zipcode  CHARACTER(8) NOT NULL,
+		address  VARCHAR(255) NOT NULL,
+		phone    VARCHAR(14)  NOT NULL,
+		website  VARCHAR(255),
+		map      VARCHAR(255)
+);
+
+/**********************************/
+/* テーブル名: 担当者連絡先 */
+/**********************************/
+CREATE TABLE contact(
+		id         SERIAL,
+		company_id INTEGER      NOT NULL,
+		name       VARCHAR(100) NOT NULL,
+		phone      VARCHAR(14)  NOT NULL,
+		email      VARCHAR(255) NOT NULL
+);
+
+/**********************************/
+/* テーブル名: 認証情報 */
+/**********************************/
+CREATE TABLE user_auth(
+		id       SERIAL,
+		name     VARCHAR(100) NOT NULL,
+		email    VARCHAR(255) NOT NULL,
+		password VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE user_auth ADD CONSTRAINT IDX_user_auth_PK PRIMARY KEY (id);
+ALTER TABLE user_auth ADD CONSTRAINT IDX_user_auth_UQ UNIQUE (email);
+
+ALTER TABLE company ADD CONSTRAINT IDX_company_PK PRIMARY KEY (id);
+
+ALTER TABLE contact ADD CONSTRAINT IDX_contact_PK PRIMARY KEY (id);
+ALTER TABLE contact ADD CONSTRAINT IDX_contact_FK FOREIGN KEY (company_id) REFERENCES company (id);
